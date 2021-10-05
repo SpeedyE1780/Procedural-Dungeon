3258c27ce6a8e7af106180136069052f84585b00
SpeedyE1780 <sandroesaab@gmail.com>
2021-10-05 16:35:00 +0300

    Remove enemy wait before moving to next waypoint

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,44 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    private const float DistanceThreshold = 2f;
+
     NavMeshAgent agent;
     List<Transform> waypoints;
     int currentIndex;
 
     public void SetEnemy(List<Transform> waypointsPositions, int index)
     {
+        SetNavAgent();
+        SetWaypoints(waypointsPositions, index);
+        agent.SetDestination(waypoints[currentIndex].position);
+    }
+
+    private void SetNavAgent()
+    {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = Random.Range(1.5f, 5);
         agent.angularSpeed = Random.Range(90f, 150f);
+    }
+
+    private void SetWaypoints(List<Transform> waypointsPositions, int index)
+    {
         waypoints = new List<Transform>();
         currentIndex = index;
 
         foreach (Transform waypoint in waypointsPositions)
-        {
             waypoints.Add(waypoint);
-        }
-
-        agent.SetDestination(waypoints[currentIndex].position);
     }
 
     private void Update()
     {
-        if (agent.remainingDistance < 2 && !agent.isStopped)
+        if (agent.remainingDistance < DistanceThreshold)
         {
             currentIndex = (currentIndex + 1) % waypoints.Count;
-            agent.isStopped = true;
-            StartCoroutine(Wait());
+            agent.SetDestination(waypoints[currentIndex].position);
         }
-    }
-
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(Random.Range(0.5f, 2));
-        agent.SetDestination(waypoints[currentIndex].position);
-        agent.isStopped = false;
     }
 }
