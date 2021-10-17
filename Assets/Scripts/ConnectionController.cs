823a0d590937f27733cbb62afaae9103b1c42d1b
SpeedyE1780 <sandroesaab@gmail.com>
2021-10-17 20:40:04 +0300

    Fix connections not activating when teleporting to different floor

--- Assets/Scripts/ConnectionController.cs
+++ Assets/Scripts/ConnectionController.cs
@@ -7,6 +7,16 @@ public class ConnectionController : MonoBehaviour
     protected Animator anim;
 
     protected virtual void Awake() => anim = GetComponent<Animator>();
+
+    //Activate connections when room are set active again
+    protected virtual void OnEnable()
+    {
+        if (anim == null)
+            return;
+
+        ActivateConnections();
+    }
+
     public virtual void UpdateConnection() { }
     public virtual void ActivateConnections() => anim.SetBool("RoomCleared", true);
 }
--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -3,27 +3,17 @@
 public class PortalController : ConnectionController
 {
     Vector3 targetPosition;
-    bool isActive;
     int floorIncrement;
 
     protected override void Awake()
     {
         base.Awake();
-        isActive = false;
         floorIncrement = connectionSide == ConnectionSide.Up ? 1 : -1;
-    }
-
-    //Play open animation again
-    private void OnEnable()
-    {
-        if (isActive)
-            base.ActivateConnections();
     }
 
     //Play open animation
     public override void ActivateConnections()
     {
-        isActive = true;
         base.ActivateConnections();
     }
 
